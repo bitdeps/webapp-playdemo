d1ad4c24b0d506fc073b9688ab8e859865619949
Denis Baryshev <dennybaa@gmail.com>
2024-01-09 18:44:42 +0700

    fix linting issues
    Signed-off-by: Denis Baryshev <dennybaa@gmail.com>

--- cmd/webapp-playdemo/main.go
+++ cmd/webapp-playdemo/main.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
+	log.Printf("Web app running on localhost:3000\n")
+
+	logger := log.New(os.Stdout, "webapp ", log.LstdFlags)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		logger.Printf("Request received for: %s\n", r.URL.Path)
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
-	fmt.Println("Web app running on localhost:3000")
-	http.ListenAndServe(":3000", nil)
+
+	server := &http.Server{
+		Addr:              ":3000",
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		logger.Fatalf("Error starting the web server: %s\n", err)
+	}
 }
